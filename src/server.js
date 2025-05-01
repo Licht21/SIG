3bdf528fe0ddff052fee162cf3eb9fdb862516a8
yusuf <yusuf210409@gmail.com>
2025-05-01 17:28:35 +0700

    test

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import { routes } from "./routes.js";
 const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 8080,
-    // host: "localhost",
+    host: "0.0.0.0",
   });
 
   server.route(routes);
